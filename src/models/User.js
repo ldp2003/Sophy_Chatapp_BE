e5c8f7fbf424db36caced85dd29616886de4da81
ldp2003 <phatpro1208@gmail.com>
2025-03-10 23:13:46 +0700

    init model

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const dynamoose = require("dynamoose");
+
+const schema = new dynamoose.Schema({
+    ID: {
+        type: String,
+        hashKey: true,
+    },
+    username: String,
+    password: String,
+    fullname: String,
+    ismale: Boolean,
+    phone: String,
+    urlavatar: String,
+    birthday: String,
+    friendList: {
+        type: Array,
+        schema: [String],
+    },
+});
+
+const User = dynamoose.model("User", schema);
+
+module.exports = User;
