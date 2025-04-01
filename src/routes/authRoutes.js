ceb4c2d4b57586e4a0344ebfc0ad486d8ac64ceb
ldp2003 <phatpro1208@gmail.com>
2025-04-01 12:44:02 +0700

    change otp-related from get to post

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth');
 const authController = new AuthController();
 
 router.post('/login', authController.login.bind(authController));
-router.get('/check-used-phone/:phone', authController.checkPhoneLimiter, authController.checkUsedPhone.bind(authController));
+router.post('/check-used-phone/:phone', authController.checkPhoneLimiter, authController.checkUsedPhone.bind(authController));
 router.post('/verify-phone-otp', authController.phoneVerificationLimiter, authController.verifyPhoneOTP.bind(authController));
 router.post('/generate-qr-token', authController.generateQrToken.bind(authController));
 router.post('/verify-qr-token', auth, authController.verifyQrToken.bind(authController));
@@ -16,7 +16,7 @@ router.post('/register', authController.register.bind(authController));
 router.post('/logout', auth, authController.logout.bind(authController));
 router.patch('/refresh', auth, authController.refreshToken.bind(authController));
 router.put('/change-password', auth, authController.changePassword.bind(authController));
-router.get('/send-otp-forgot-password', authController.forgotPasswordLimiter, authController.sendOTPForgotPassword.bind(authController));
+router.post('/send-otp-forgot-password', authController.forgotPasswordLimiter, authController.sendOTPForgotPassword.bind(authController));
 router.post('/verify-otp-forgot-password', authController.forgotPasswordVerificationLimiter ,authController.verifyPhoneOTP.bind(authController));
 router.put('/forgot-password', authController.forgotPassword.bind(authController));
 
